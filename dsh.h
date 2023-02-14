020f5769554e29155bbb1c158eb5ab335f847dec
Mitch <mdhurley@pugetsound.edu>
2023-02-14 20:11:51 +0000

    initial scripting for external programs

--- dsh.h
+++ dsh.h
@@ -8,6 +8,8 @@
 #define MAXBUF 256
 #define HISTORY_LEN 100
 #define DSH_PROMPT "dsh> "
+#define DELIM " "
+#define MAX_ARGS 25
 
 // TODO: Your function prototypes below
 void mode1exe(char *path);
--- main.c
+++ main.c
@@ -15,13 +15,65 @@
 int main(int argc, char **argv)
 {
 	char cmdline[MAXBUF]; // stores user input from commmand line
+	char path[MAXBUF];
+	cmd_t currComm;
+	
+	
 	//print the shell bit
-	printf(DSH_PROMPT);
-	fgets(cmdline, MAXBUF, stdin);
-	//trim input
-	while (cmdline[0] != '/0'){
+	do {
+		printf(DSH_PROMPT);
+		fgets(cmdline, MAXBUF, stdin);
+		//trim input
+		strcpy(cmdline, trimWhitespace());
+		//if the first character is forward slash, use mode1
+		char *ptr = strtok(cmdline, " ");
+		char *args[MAX_ARGS + 1];
+		//gets the first arg
+		*args[0] = strdup(ptr);
+		i = 1;
+		//read in the command line input and put it in 
+		while (ptr != NULL && i < MAX_ARGS){
+			args[i++] = strtok(NULL, DELIM);
+		}
+		//get the 
+		currComm = chkBuiltin(*args[0]);
+		//case of an external command
+		if (currComm == CMD_EXT){
+			//declare array of arguments
+			
+			//marks end of list
+			args[i] = NULL;
+			int pid = fork();
+			//if its the child process
+			if (pid == 0){
+				execv(args[0], args);
+			}
+			else if (pid > 0) {
+			// parent process
+				wait(&status);
+			} else {
+				// fork failed
+				perror("fork");
+				exit(1);
+			}
 
-	}
+		}else if (currComm == CMD_CD){
+			
+		}else if (currComm == CMD_EXIT){
+			
+		}else if (currComm == CMD_PWD){
+			
+		}else if (currComm == CMD_HISTORY){
+			
+		}else if (currComm == CMD_ECHO){
+			
+		}
+		free(args[0]);
+
+		
+	} while (cmdline[0] != '/0')
+		
+	
 	//if input is empty reprompt
 
 	return 0;
